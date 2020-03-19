9300132ebd32173f684a4ea57a036a49c240a80a
Zaharov Dmitriy <d.zaharov@paymo.ru>
2020-03-19 19:54:47 +0300

    refactoring unit test

--- lists/tests.py
+++ lists/tests.py
@@ -2,20 +2,12 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
     '''home page test'''
 
-    def test_root_url_resolves_to_home_page_view(self):
-        '''test: root url resolves to home page'''
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_corrct_html(self):
+    def test_uses_home_template(self):
         '''test: home page return correct html'''
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')

6fcab37d4669400049d3162895428d5ee0a613c5
Zaharov Dmitriy <d.zaharov@paymo.ru>
2020-03-19 18:33:49 +0300

    First unit test

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    '''Smoke test'''
+class HomePageTest(TestCase):
+    '''home page test'''
 
-    def test_bad_maths(self):
-        '''wrong math test'''
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        '''test: root url resolves to home page'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)

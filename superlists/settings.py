b5bf437fbf4ea045e2d9d133aa9f962154d1ddd0
Zaharov Dmitriy <d.zaharov@paymo.ru>
2020-03-25 16:23:29 +0300

    use form

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vkr#t^b@=xcw_4ls@)cai(ewte=-*n69qlc^5b58%4ky&6(5cg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.ngrok.io']
+ALLOWED_HOSTS = []
 
 
 # Application definition

ace6a44a3c09f0cf6235d862a76f5328f5e3defb
Zaharov Dmitriy <d.zaharov@paymo.ru>
2020-03-28 20:26:41 +0300

    add add_list_item method

--- functional_test/server_tools.py
+++ functional_test/server_tools.py
@@ -8,12 +8,12 @@ def _get_manage_dot_py(host):
 
 def reset_database(host):
     manage_dot_py = _get_manage_dot_py('superlists-staging')
-    with settings(user='dmitriy', port=8022, hosts='127.0.0.1', password='1'):
+    with settings(host_string='dmitriy@localhost', port=8022, password='1'):
         run(f'{manage_dot_py} flush --noinput')
 
 
 def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py('superlists-staging')
-    with settings(user='dmitriy', port=8022, hosts='127.0.0.1', password='1'):
+    with settings(host_string='dmitriy@localhost', port=8022, password='1'):
         session_key = run(f'{manage_dot_py} create_session {email}')
         return session_key
--- functional_test/test_list_item_validation.py
+++ functional_test/test_list_item_validation.py
@@ -45,9 +45,7 @@ class ItemValidationTest(FunctionalTest):
         '''test: cannot add duplicate elements'''
         # Эдит открывает домашнюю страницу и начинает новый список
         self.browser.get(self.live_server_url)
-        self.get_item_input_box().send_keys('Buy wellies')
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy wellies')
+        self.add_list_item('Buy wellies')
 
         # Она случайно пытается ввести повторяющийся элемент
         self.get_item_input_box().send_keys('Buy wellies')
@@ -61,9 +59,7 @@ class ItemValidationTest(FunctionalTest):
         '''test: error messages are cleared on input'''
         # Эдит начинает список и вызывает ошибку валидации:
         self.browser.get(self.live_server_url)
-        self.get_item_input_box().send_keys('Banter to thick')
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Banter to thick')
+        self.add_list_item('Banter to thick')
         self.get_item_input_box().send_keys('Banter to thick')
         self.get_item_input_box().send_keys(Keys.ENTER)
 
